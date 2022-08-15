4da81b98a98b625275ac21b4fc8a16d542ee5e96
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-08-16 02:14:32 +0500

    Lesson-3 modul-3

---
+++ Uyga-Vazifalarim/lesson-3/JavaScript/style.js
@@ -0,0 +1,243 @@
+"use strict"; // qatiy rejim //
+
+// 1- misol //
+// const day = 1;
+// switch (day) {
+//     case 1:
+//         console.log(' Dushanba ');
+//         break;
+//     case 2:
+//         console.log(' Seshanba ');
+//         break;
+//     case 3:
+//         console.log(' Chorshanba ');
+//         break;
+//     case 4:
+//         console.log(' Payshanba ');
+//         break;
+//     case 5:
+//         console.log(' Juma ');
+//         break;
+//     case 6:
+//         console.log(' Shanba ');
+//         break;
+//     case 7:
+//         console.log(' Yakshanba ');
+//         break;
+//     default:
+//         console.log('Bunaqa kun yoq');
+
+// }
+
+// 2- misol //
+// const baxo = 1;
+
+// switch (baxo) {
+//     case 1:
+//         console.log('yomon')
+//         break;
+//       case 2:
+//         console.log('qoniqarsiz')
+//         break;
+//       case 3:
+//         console.log('qoniqarli')
+//         break;
+//       case 4:
+//         console.log('yaxshi')
+//           break;
+//             case 5:
+//         console.log('alo')
+//           break;
+// }
+
+// 3- misol //
+// const oy = 7;
+// switch (oy) {
+//          case 1:
+//         console.log('qish')
+//         break;
+//       case 2:
+//         console.log('qish')
+//         break;
+//       case 3:
+//         console.log('qish')
+//         break;
+//      case 4:
+//         console.log('baxor')
+//         break;
+//       case 5:
+//         console.log('baxor')
+//         break;
+//       case 6:
+//         console.log('baxor')
+//         break;
+//      case 7:
+//         console.log('yoz')
+//         break;
+//       case 8:
+//         console.log('yoz')
+//         break;
+//       case 9:
+//         console.log('yoz')
+//         break;
+//      case 10:
+//         console.log('kuz')
+//         break;
+//       case 11:
+//         console.log('kuz')
+//         break;
+//       case 12:
+//         console.log('kuz')
+//         break;
+// }
+
+// 4- misol //
+// const oy = 7;
+// switch (oy) {
+//          case 1:
+//         console.log('Yanvar 31')
+//         break;
+//       case 2:
+//         console.log('Fevral 28')
+//         break;
+//       case 3:
+//         console.log('Mart 31')
+//         break;
+//      case 4:
+//         console.log('Aprel 30')
+//         break;
+//       case 5:
+//         console.log('May 31')
+//         break;
+//       case 6:
+//         console.log('Iyun 30')
+//         break;
+//      case 7:
+//         console.log('Iyul 31')
+//         break;
+//       case 8:
+//         console.log('Avgust 31')
+//         break;
+//       case 9:
+//         console.log('Sentyabr 30')
+//         break;
+//      case 10:
+//         console.log('Oktyabr 31')
+//         break;
+//       case 11:
+//         console.log('Noyabr 30')
+//         break;
+//       case 12:
+//         console.log('Dekabr 31')
+//         break;
+// }
+
+// 5- misol //
+// let c = +prompt('amalni kiriting')
+// let a = +prompt('amalni tanlanng')
+// let b = +prompt('amalni kiriting')
+
+// switch (c) {
+//     case "-":
+//         console.log(a - b);
+//           break;
+//           case "+":
+//         console.log(a + b);
+//           break;
+//           case "/":
+//         console.log(a / b);
+//           break;
+//           case "*":
+//         console.log(a * b);
+//           break;
+
+// }
+
+// 6- misol //
+// const baxo = 5;
+
+// switch (baxo) {
+//     case 1:
+//         console.log("100-ming so'm")
+//            break;
+//           case 2:
+//         console.log("200-ming so'm")
+//            break;
+//           case 3:
+//         console.log("300-ming so'm")
+//            break;
+//           case 4:
+//         console.log("400-ming so'm")
+//            break;
+//           case 5:
+//         console.log("500-ming so'm")
+//            break;
+// }
+
+// 7- misol //
+// let k = 10;
+// let n = 5;
+// for (let i = 0; i < n; i++ ){
+//     console.log(k);
+// }
+
+// 8-misol
+// let a = 10;
+// let b = 5;
+
+// // let length = a - b;
+
+// for(let i = a; i >= b; i--){
+//     console.log(i);
+// }
+
+// 9-misol
+
+// let a = 10;
+// let b = 5;
+
+// for(let i = a -1; i >= b + 1; i--){
+//     console.log(i);
+// }
+
+// 10-misol
+
+// let kilo = 10;
+
+// for(let i = 1; i <= kilo; i++){
+//     console.log(${i * kilo} ${i * 10000} so'm);
+// }
+
+// 11-misol
+// let sum = 0;
+// let a = 2;
+// let b = 10;
+
+// for(let i = a; i <= b; i++ ){
+//     sum += i ;
+// }
+
+// console.log(sum);
+
+// 12-misol
+
+// let sum = 1;
+// let a = 2;
+// let b = 10;
+
+// for(let i = a; i <= b; i++ ){
+//     sum *= i ;
+// }
+
+// console.log(sum);
+
+// 13-misol
+
+// let n = 10;
+// let sum = 0;
+// let factorial = 1;
+
+// for(let i = 1; i <= n; i++){
+//     factorial *= i ;
+
+// }
